e5337411f55247c40f98ae59de9eb494609bc078
tiagombp <tiagombp@gmail.com>
2019-09-20 16:27:52 -0300

    cria função de resize

--- visualizacao.js
+++ visualizacao.js
@@ -1,3 +1,17 @@
 const $grafico = d3.select('.grafico-d3');
 const $grafico_container = d3.select('.grafico-d3-container');
 const $svg     = $grafico.select('.grafico-d3-svg');
+
+// Define uma função que vai recalcular a largura disponível para o gráfico conforme o tamanho do div de container.
+
+function resize() {
+
+    console.log('estou em resize'); // só para mostrar que está funcionando.
+
+    // captura largura do div que envolve o svg
+    const w = $grafico_container.node().offsetWidth;
+
+    // agora chama o render toda vez que mudar o tamanho, passando a largura:
+    render(w);
+
+}
