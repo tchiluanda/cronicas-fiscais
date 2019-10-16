7808f80ffe6e0addeac98f0d9242e311effc4ad0
tiagombp <tiagombp@gmail.com>
2019-10-16 13:45:56 -0300

    reorganiza código, cria função para renderizar o primeiro step

--- visualizacao.js
+++ visualizacao.js
@@ -79,26 +79,98 @@ d3.csv("dados.csv", function(d) {
                    .attr("width", w)
                    .attr("height", h);
 
-    // // // Step 1 - Valores e caixa de texto
+    // funcoes 
 
-    const dados_pontos = dados.filter(d => d.periodo >= LAST_DATE);
-    // não sei por que só funcionou aqui com >=, se não ele só traz uma linha)
+    const switch_step = function(step) {
+        d3.selectAll(".steps li").classed("active", false);
+        d3.select("#step-" + step).classed("active", true);
+    }
     
-    console.log("Dados pontos");
-    console.table(dados_pontos);
+    // // // Step 1 - Valores e caixa de texto
+    const render_step1 = function(dados) {
+        const dados_pontos = dados.filter(d => d.periodo >= LAST_DATE);
+        // não sei por que só funcionou aqui com >=, se não ele só traz uma linha)
+    
+        console.log("Dados pontos");
+        console.table(dados_pontos);
+  
+        const v_pontos = $SVG.selectAll("circle")
+                                .data(dados_pontos)
+                                .enter()
+                                .append("circle")
+                                .attr("cx", d => scale_Y_PERIODO(d.periodo))
+                                .attr("cy", d => scale_ABSOLUTO(d.vlr_acu)+25)
+                                .attr("r", 0)
+                                .attr("fill", function(d) {
+                                    if (d.tipo_despesa == "obrigatoria") return "steelblue"
+                                    else return "lightcoral"})
+                                .transition()
+                                .duration(1000)
+                                .attr("r", 10)
+                                .attr("cy", d => scale_ABSOLUTO(d.vlr_acu))
+    
+    }
+    
+    
+    
+    // // // Step 2 - Linhas                    
+    const render_step2 = function(dados) {
+        
+        // create line
+        const line_acum = d3.line()
+        .x(d => scale_Y_PERIODO(d.periodo))
+        .y(d => scale_ABSOLUTO(d.vlr_acu));
+    
+        const dados_obrig = dados.filter(d => d.tipo_despesa == "obrigatoria");
+        const dados_discr = dados.filter(d => d.tipo_despesa == "discricionaria");
+    
+        console.table(dados_obrig);
+        console.table(dados_discr);
+  
+        const t_linhas = 3000;
+    
+        const v_linha_obrig = $SVG.append("path")
+                    .datum(dados_obrig)
+                    .attr("class", "line obrig")
+                    .attr("d", line_acum)
+                    .attr('stroke', "steelblue")
+                    .attr('stroke-width', 3)
+                    .attr('fill', 'none');
+    
+        let comprimento_linha_obrig = v_linha_obrig.node().getTotalLength();
+        console.log("Comprimento linha obrig:", comprimento_linha_obrig);
+    
+        v_linha_obrig
+        .attr("stroke-dasharray", comprimento_linha_obrig + " " + comprimento_linha_obrig)
+        .attr("stroke-dashoffset", comprimento_linha_obrig)
+        .transition()
+        .duration(t_linhas)
+        .ease(d3.easeLinear)
+        .attr("stroke-dashoffset", 0);
+    
+        const v_linha_discr = $SVG.append("path")
+                    .datum(dados_discr)
+                    .attr("class", "line discr")
+                    .attr("d", line_acum)
+                    .attr('stroke', "lightcoral")
+                    .attr('stroke-width', 3)
+                    .attr('fill', 'none');
+    
+        let comprimento_linha_discr = v_linha_discr.node().getTotalLength();
+        console.log("Comprimento linha discr:", comprimento_linha_discr);
+    
+        v_linha_discr
+        .attr("stroke-dasharray", comprimento_linha_discr + " " + comprimento_linha_obrig)
+        .attr("stroke-dashoffset", comprimento_linha_discr)
+        .transition()
+        .duration(t_linhas)
+        .ease(d3.easeLinear)
+        .attr("stroke-dashoffset", 0);
+    }
 
-    const v_pontos = $SVG.selectAll("circle")
-                         .data(dados_pontos)
-                         .enter()
-                         .append("circle")
-                         .attr("cx", d => scale_Y_PERIODO(d.periodo))
-                         .attr("cy", d => scale_ABSOLUTO(d.vlr_acu))
-                         .attr("r", 0)
-                         .attr("fill", function(d) {
-                             if (d.tipo_despesa == "obrigatoria") return "steelblue"
-                             else return "lightcoral"})
-                        .transition(3000)
-                        .attr("r", 6);
+    // inicio fluxo
+    
+    render_step1(dados);
 
     d3.selectAll(".nav-stepper li")
       .on("click", function(){
@@ -120,6 +192,9 @@ d3.csv("dados.csv", function(d) {
           switch_step(step_atual);
 
           switch (""+step_atual) {
+            case "1":
+                render_step1(dados);
+                break;              
             case "2":
                 render_step2(dados);
                 break;
@@ -129,70 +204,7 @@ d3.csv("dados.csv", function(d) {
 
       })
 
-
-const switch_step = function(step) {
-    d3.selectAll(".steps li").classed("active", false);
-    d3.select("#step-" + step).classed("active", true);
-}
-
-const render_step2 = function(dados) {
-    // // // Step 2 - Linhas
-
-    // create line
-    const line_acum = d3.line()
-    .x(d => scale_Y_PERIODO(d.periodo))
-    .y(d => scale_ABSOLUTO(d.vlr_acu));
-
-    const dados_obrig = dados.filter(d => d.tipo_despesa == "obrigatoria");
-    const dados_discr = dados.filter(d => d.tipo_despesa == "discricionaria");
-
-    console.table(dados_obrig);
-    console.table(dados_discr);
-
-    const t_linhas = 3000;
-
-    const v_linha_obrig = $SVG.append("path")
-                .datum(dados_obrig)
-                .attr("class", "line obrig")
-                .attr("d", line_acum)
-                .attr('stroke', "steelblue")
-                .attr('stroke-width', 3)
-                .attr('fill', 'none');
-
-    let comprimento_linha_obrig = v_linha_obrig.node().getTotalLength();
-    console.log("Comprimento linha obrig:", comprimento_linha_obrig);
-
-    v_linha_obrig
-    .attr("stroke-dasharray", comprimento_linha_obrig + " " + comprimento_linha_obrig)
-    .attr("stroke-dashoffset", comprimento_linha_obrig)
-    .transition()
-    .duration(t_linhas)
-    .ease(d3.easeLinear)
-    .attr("stroke-dashoffset", 0);
-
-    const v_linha_discr = $SVG.append("path")
-                .datum(dados_discr)
-                .attr("class", "line discr")
-                .attr("d", line_acum)
-                .attr('stroke', "lightcoral")
-                .attr('stroke-width', 3)
-                .attr('fill', 'none');
-
-    let comprimento_linha_discr = v_linha_discr.node().getTotalLength();
-    console.log("Comprimento linha discr:", comprimento_linha_discr);
-
-    v_linha_discr
-    .attr("stroke-dasharray", comprimento_linha_discr + " " + comprimento_linha_obrig)
-    .attr("stroke-dashoffset", comprimento_linha_discr)
-    .transition()
-    .duration(t_linhas)
-    .ease(d3.easeLinear)
-    .attr("stroke-dashoffset", 0);
-}
-
-
-
-console.log("Step atual:", step_atual);
+    console.log("Step atual:", step_atual);
 
 
 
